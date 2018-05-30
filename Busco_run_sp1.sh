cfa0c0bc3ec38eca0c62623b1f5770e4f2880ced
Benjamin-Guinet <39563212+Benjamin-Guinet@users.noreply.github.com>
2018-05-30 13:16:38 +0200

    Add files via upload

---
+++ Busco_run_sp1.sh
@@ -0,0 +1,70 @@
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=8:bigmem,mem=30gb
+#PBS -e /pandata/bguinet/LEPIWASP/LOGS/Busco_sp1.error
+#PBS -o /pandata/bguinet/LEPIWASP/LOGS/Busco_sp1.out
+#PBS -N run_busco_sp1
+#PBS -q q1day
+
+
+# This script allows you to run the Busco program (search for ultra-conserved genes
+# in the genome in one single copy) in one species;
+# This allows both an idea of the quality of assembly,
+# and to have a distribution under the null hypothesis of divergence between pairs of sequences
+# for genes that have been transmitted by vertical transfer only.
+
+# Required files:
+# - Assembled genome sp1 format fasta (assembly)
+# - Species of Interest Database (lineage) (to be found on http://busco.ezlab.org/)
+# - Busco Outbuildings (Augustus, HMM, Blast, Python)
+
+# Be sure to define variable names and paths to dependencies and files before issuing commands.
+
+hostname
+uname -a
+
+echo debut:
+date  #debut 
+
+#Your samp name
+SAMP=sp1
+
+# run BUSCO on genome assembly
+
+#######################
+# define path to data #
+#######################
+# GenomeE (sequence file)
+ASSEMBLY=/path/to/your/data/$SAMP/scaffold.fa
+# Species data 
+LINEAGE=/path/to/your/data/arthropoda_odb9/
+# Name output dir
+NAME=$SAMP'_BUSCO_v2' # WARNING: do not provide a path.
+
+
+#########################################
+# define PATH to sofwtare used by BUSCO #
+#########################################
+#Augustus
+export PATH=/bin:/usr/bin:/usr/remote/bin:/panhome/varaldi/TOOLS/augustus-3.2.3/
+# hmmer
+PATH=$PATH:/usr/remote/hmmer-3.1b2/bin
+# blast et python
+PATH=$PATH:/usr/bin
+PATH=$PATH:/panusr/ncbi-blast-2.2.25+/bin/
+# augustus
+#PATH=$PATH:/panhome/varaldiTOOLS/augustus-3.2.3/bin/ 
+
+export AUGUSTUS_CONFIG_PATH=/panhome/varaldi/TOOLS/augustus-3.2.3/config
+
+#Busco software path
+BUSCO=/panhome/varaldi/TOOLS/BUSCO_v2/BUSCO.py
+
+################
+# Command line #
+################
+python $BUSCO -i $ASSEMBLY -o $NAME -l $LINEAGE -m geno -f
+
+
+echo fin:
+date  #fin 
+
---
+++ augustus_run_sp2_training_sp2.sh
@@ -0,0 +1,56 @@
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=8:bigmem,mem=30gb
+#PBS -e /pandata/bguinet/LEPIWASP/ACG-sp2_Augustus/LOGS/augustus_training_sp2.error
+#PBS -o /pandata/bguinet/LEPIWASP/ACG-sp2_Augustus/LOGS/augustus_training_sp2.out
+#PBS -q q1day
+#PBS -N augustus_sp2_training_sp2
+
+
+# Prediction of genes in the genome of species 2 using its own training.
+
+# This script is used to run the Augustus program that will predict genes by an Ab initio approach.
+# With Busco run,  training files are available to better predict genes.
+# Note that this retraining file (myspecie) must be added to the species file of the Augustus program.
+
+# Usual Busco retraining file path: sp2_busco / run_sp2_BUSCO_v2 / augustus_output / retraining_parameters
+# The usual path of the file where to transfer the retraining file: / augustus / config / species / myspecie
+
+# Required files:
+# - Genome Assembled Fasta Format (assembly)
+# - Training file for species 2 (retraining)
+
+
+# Be sure to define variable names and file paths before issuing commands.
+
+
+echo debut:
+date  #debut 
+
+hostname
+uname -a
+
+# 
+# 
+
+#Declarations des variables 
+SAMP=sp2
+PATH=bguinet@pbil-deb
+
+# GENOME (sequence file)
+ASSEMBLY=/pandata/varaldi/LEPIWASP/OUT/$SAMP/scaffold.fa
+
+# augustus config path (where the retraining file from busco should be added in the specie file)
+PATH=/panhome/bguinet/TOOLS/augustus/config/
+
+#retraining file's name (not a path) 
+RETRAINING=retraining_sp2
+#Augustus programme path
+AUGUSTUS=/panhome/bguinet/TOOLS/augustus/bin/augustus
+
+
+# outputfile's path and name
+OUTPUT=/pandata/bguinet/LEPIWASP/ACG-sp2_Augustus/LOGS/run_augustus_sp2_training_sp2.out
+
+$AUGUSTUS --species=$RETRAINING --AUGUSTUS_CONFIG_PATH=$PATH $ASSEMBLY > $OUTPUT
+
+
